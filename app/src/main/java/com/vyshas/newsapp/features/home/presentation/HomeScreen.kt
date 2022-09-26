6fdcfa89e8ae3fb4affe09fbd49462c63dd71d61
vyshakh.shanthi <vyshakh.shanthi@dojo.tech>
2022-09-26 01:22:30 +0100

    - add theme, colors and type based on material 3. - add fonts. - update dependencies. - add sample list item.

--- app/src/main/java/com/vyshas/newsapp/features/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/vyshas/newsapp/features/home/presentation/HomeScreen.kt
@@ -1,8 +1,154 @@
 package com.vyshas.newsapp.features.home.presentation
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.vyshas.newsapp.features.home.domain.entity.TopHeadlinesEntity
+import com.vyshas.newsapp.features.home.domain.entity.previewTopHeadlinesEntity
+import com.vyshas.newsapp.ui.theme.NewsAppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun HomeListScreen(
+    topHeadlinesList: List<TopHeadlinesEntity>,
+    modifier: Modifier = Modifier
+) {
+    val topAppBarState = rememberTopAppBarState()
+    Scaffold(
+        modifier = modifier
+    ) { innerPadding ->
+        HomeListContent(
+            modifier = modifier,
+            contentPadding = innerPadding,
+            topHeadlinesList = topHeadlinesList
+        )
+    }
+}
 
 @Composable
-private fun HomeScreenWithList(){
+fun HomeListContent(
+    topHeadlinesList: List<TopHeadlinesEntity>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    state: LazyListState = rememberLazyListState()
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = contentPadding,
+        state = state
+    ) {
+        item {
+            HomeNewsItemSection(topHeadlinesList = topHeadlinesList)
+        }
+    }
+}
 
+@Composable
+fun HomeNewsItemSection(
+    topHeadlinesList: List<TopHeadlinesEntity>
+) {
+    Column {
+        topHeadlinesList.forEach { topHeadlinesEntity ->
+            NewsCardSimple(
+                topHeadlinesEntity = topHeadlinesEntity
+            )
+            NewsListDivider()
+        }
+    }
+}
+
+@Composable
+fun NewsListDivider() {
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewsCardSimple(
+    modifier: Modifier = Modifier,
+    topHeadlinesEntity: TopHeadlinesEntity
+) {
+    ListItem(
+        headlineText = {
+            Title(topHeadlinesEntity = topHeadlinesEntity)
+        },
+        supportingText = {
+            SupportingText(topHeadlinesEntity)
+        }
+    )
+}
+
+@Composable
+fun SupportingText(topHeadlinesEntity: TopHeadlinesEntity) {
+    Text(
+        text = topHeadlinesEntity.description,
+        style = MaterialTheme.typography.bodyMedium,
+        maxLines = 3,
+        overflow = TextOverflow.Visible,
+    )
+}
+
+@Composable
+fun Title(topHeadlinesEntity: TopHeadlinesEntity) {
+    Text(
+        text = topHeadlinesEntity.name,
+        style = MaterialTheme.typography.titleMedium,
+        maxLines = 3,
+        overflow = TextOverflow.Ellipsis,
+    )
+}
+
+/**
+ * Display an initial empty state or swipe to refresh content.
+ *
+ * @param empty (state) when true, display [emptyContent]
+ * @param emptyContent (slot) the content to display for the empty state
+ * @param loading (state) when true, display a loading spinner over [content]
+ * @param onRefresh (event) event to request refresh
+ * @param content (slot) the main content to show
+ */
+@Composable
+private fun LoadingContent(
+    empty: Boolean,
+    emptyContent: @Composable () -> Unit,
+    loading: Boolean,
+    onRefresh: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    if (empty) {
+        emptyContent()
+    } else {
+        SwipeRefresh(
+            state = rememberSwipeRefreshState(loading),
+            onRefresh = onRefresh,
+            content = content,
+        )
+    }
+}
+
+@Preview("Simple News card")
+@Preview("Simple News card (dark)", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun SimpleTopHeadlinesPreview() {
+    NewsAppTheme() {
+        Surface {
+            NewsCardSimple(topHeadlinesEntity = previewTopHeadlinesEntity[0])
+        }
+    }
 }
--- app/src/main/java/com/vyshas/newsapp/ui/theme/Type.kt
+++ app/src/main/java/com/vyshas/newsapp/ui/theme/Type.kt
@@ -1,28 +1,124 @@
 package com.vyshas.newsapp.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.vyshas.newsapp.R
 
-// Set of Material typography styles to start with
+//Replace with your font locations
+val SourceCodePro = FontFamily(
+    Font(R.font.sourcecodeproregular),
+    Font(R.font.sourcecodepromedium, FontWeight.Medium),
+    Font(R.font.sourcecodeprobold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+    displayLarge = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.Light,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = 0.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayMedium = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.Light,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = SourceCodePro,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = SourceCodePro,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
     )
-    */
 )
