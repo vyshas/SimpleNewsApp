fee870a83d63757df4ad3ee4105eb2ee399bf06e
vyshakhshanthi <vyshas@gmail.com>
2023-01-23 19:47:38 +0000

    - setup for sports news

--- app/src/main/java/com/vyshas/newsapp/features/sports/SportsFragment.kt
+++ app/src/main/java/com/vyshas/newsapp/features/sports/SportsFragment.kt
@@ -1,4 +1,4 @@
-package com.vyshas.newsapp.features.explore
+package com.vyshas.newsapp.features.sports
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.vyshas.newsapp.R
 
-class ExploreFragment : Fragment() {
+class SportsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +19,6 @@ class ExploreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        return inflater.inflate(R.layout.fragment_sports, container, false)
     }
 }
